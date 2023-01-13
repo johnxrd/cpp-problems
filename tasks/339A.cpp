11e8eaef9b856f2b5324715c2081cd9e2bedc778
johnxrd <cebotari.john@gmail.com>
2023-01-13 12:33:28 +0200

    add solution for the task

---
+++ tasks/339A.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/339/A
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+    string a; 
+    cin>>a;
+	
+    int z=a.length();
+    
+    for(int i=0 ;i<z-1; i+=2)
+    for(int j=i+2;j<z;j+=2)
+    if(a[j]<a[i])
+    swap(a[j],a[i]);
+	cout<<a;
+
+	return 0;
+}
