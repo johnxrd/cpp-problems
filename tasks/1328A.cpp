1646b55960ca1a7a0389f039d8a0f26fbbae05bc
johnxrd <cebotari.john@gmail.com>
2023-01-20 08:02:37 +0200

    add solved problem

---
+++ tasks/1328A.cpp
@@ -0,0 +1,9 @@
+//https://codeforces.com/problemset/problem/1328/A
+
+#include<iostream>
+using namespace std; 
+int t,a,b;
+main(){
+    for(cin>>t;t--;cout<<(b-a%b)%b<<endl) 
+    cin>>a>>b;
+    }
